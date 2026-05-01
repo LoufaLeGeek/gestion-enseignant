8ae6afc2ee2f2a1fb34658097ad0c1761a18a0ea
LoufaLeGeek <fthiam23344@gmail.com>
2026-05-01 23:34:56 +0000

    fix: ajout de TP dans le type des cours

--- app/Enums/TypeCoursEnum.php
+++ app/Enums/TypeCoursEnum.php
@@ -6,12 +6,14 @@ enum TypeCoursEnum: string
 {
     case CM = 'CM';
     case TD = 'TD';
+    case TP = 'TP';
 
     public function label(): string
     {
         return match($this) {
             self::CM => 'Cours Magistral',
             self::TD => 'Travaux Dirigés',
+            self::TP => 'Travaux pratiques',
         };
     }
 }
